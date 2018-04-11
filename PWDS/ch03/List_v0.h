2f4f190006cfc9cb779f38154e22588a2fcee06e
Lambda <2861429309@qq.com>
2018-04-11 21:47:59 +0800

    Desktop add 201804112148

---
+++ PWDS/ch03/List_v0.h
@@ -0,0 +1,89 @@
+
+
+// 线性表的顺序存储结构
+// 只能存储内建类型
+
+#ifndef LIST_V0_H
+#define LIST_V0_H
+
+#include <stddef.h> // size_t
+
+#include <algorithm> // std::find()
+#include <iterator> // std::begin() std::end()
+
+template<typename T, size_t S>
+class List_v0 
+{
+public:
+    List_v0()
+        : size_(0), capcity_(S)
+    {}
+
+    bool empty() const 
+    {
+        return size_ == 0;
+    }
+
+    void clear() 
+    {
+        size_ = 0;
+    }
+
+    // 不检查是否越界
+    T get(size_t index) const 
+    {
+        return container_[index];
+    }
+
+    size_t find(T value) const 
+    {
+        return std::end(container_) - std::find(std::begin(container_), std::end(container_), value) ;
+    }
+
+    // 不检查越界
+    void push(T value)
+    {
+        container_[size_] = value;
+        ++size_;
+    }
+
+    // 不检查越界
+    void insert(size_t index, T value)
+    {
+        for (int i = size_; i > index; --i)
+        {
+            container_[i+1] = container_[i];
+        }
+        container_[index] = value;
+        ++size_;
+    }
+    // 不检查越界
+    T deletel(size_t index)
+    {
+        T value = container_[index];
+        for (int i = index; i < size_; ++i)
+        {
+            container_[i] = container_[i+1];
+        }
+        --size_;
+        return value;
+    }
+
+    size_t size() const 
+    {
+        return size_;
+    }
+
+    size_t capcity() const 
+    {
+        return capcity_;
+    }
+
+private: 
+    size_t size_;
+    size_t capcity_;
+    T container_[S];
+};
+
+
+#endif // !LIST_V0_H
---
+++ PWDS/ch03/list_test.cpp
@@ -0,0 +1,30 @@
+
+
+#include "List_v0.h"
+#include <iostream>
+
+
+int main(void)
+{
+    std::cout << "Test list" << std::endl;
+
+    List_v0<int, 32> l1;
+    std::cout << "list size: " << l1.size() << " capcity: " << l1.capcity() << std::endl;
+
+    l1.push(2);
+    l1.push(6);
+    l1.push(1);
+    std::cout << "list size: 3 --> " << l1.size() << "\n";
+
+    l1.insert(0, 0);
+    std::cout << "list get(0): 0 --> " << l1.get(0) << "\n";
+
+    std::cout << "list find(6): true -->  " << (l1.find(6) == 0 ? "false" : "true") << "\n";
+
+    int ret = l1.deletel(0);
+    std::cout << "list deletel(0) value: 0 -->  " << ret << "\n";
+
+    l1.clear();
+    std::cout << "list clear size: 0 --> " << l1.size() << "\n";
+    return 0;
+}
