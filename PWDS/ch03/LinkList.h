f40d6947d074e31356724be9a5924b5bcb5b6956
Lambda <2861429309@qq.com>
2018-04-14 15:21:34 +0800

    Desktop add 2018-04-14 15:21:23

--- PWDS/ch03/LinkList.h
+++ PWDS/ch03/LinkList.h
@@ -9,14 +9,12 @@
 
 #include <stddef.h>
 
-#include <iostream>
-
-
 template<typename T>
 class LinkList 
 {
 public: 
     //typedef T* iterator;
+    using iterator = T*;
 
     LinkList()
         : head_(0), iterator_node_(NULL)
@@ -28,197 +26,20 @@ public:
         clear();
     }
 
-    // 头插法
-    void push(T value)
-    {
-        Node *tmp = new Node(value);
-        tmp->next_ = head_.next_;
-        head_.next_ = tmp;
-    }
+    void push(const T &value);
+    void append(const T &value);
+    void pop(T &ret);
+    void insert(size_t i, const T &value);
+    void deletel(size_t i, T &ret);
+    T& get(size_t i);
 
-    void append(T value)
-    {
-        Node *node = &head_;
-        while (node->next_)
-        {
-            node = node->next_;
-        }
+    void iterator_begin(size_t i = 1);
+    bool iterator_end() const;
+    iterator iterator_next();
 
-        Node *tmp = new Node(value);
-        node->next_ = tmp;
-    }
-
-    // 取第一个节点
-    T pop()
-    {
-        Node *tmp = head_.next_;
-        head_.next_ = tmp->next_;
-        T t = tmp->data_;
-        delete tmp;
-
-        return t;
-    }
-
-    // 如果索引大于链表大小 直接把新节点插入链尾
-    void insert(size_t i, T value)
-    {
-        Node *node = &head_;
-        size_t j = 1;
-        while (node->next_ && j < i)
-        {
-            node = node->next_;
-            ++j;
-        }
-
-        Node *tmp = new Node(value);
-        if (!node->next_ || j > i) // 最后一个节点
-        {
-            node->next_ = tmp;
-        }
-        else 
-        {
-            tmp->next_ = node->next_->next_;
-            node->next_ = tmp;
-        }
-    }
-
-    // 删除索引处的节点
-    void deletel(size_t i, T &ret)
-    {
-        Node *node = &head_;
-        size_t j = 1;
-        while (node->next_ && j < i)
-        {
-            node = node->next_;
-            ++j;
-        }
-
-        if (!node->next_ || j > i) // 最后一个节点
-        {
-            return;
-        }  
-        else 
-        {
-            Node *tmp = node->next_;
-            node->next_ = tmp->next_;
-            ret = tmp->data_;
-            delete tmp;
-        }      
-    }
-
-    // 如果索引大于链表大小 直接返回最后节点
-    T get(size_t i)
-    {
-        Node *node = &head_;
-        size_t j = 1;
-        while (node->next_ && j < i)
-        {
-            node = node->next_;
-            ++j;
-        }
-
-        if (!node->next_ || j > i)
-        {
-            return node->data_;
-        }
-        else 
-        {
-            return node->next_->data_ ;
-        }
-    }
-
-    // 单链表简单迭代器
-    void iterator_init(size_t i = 1)
-    {
-        Node *node = head_.next_;
-        size_t j = 1;
-        while (node && j < i)
-        {
-            node = node->next_;
-            ++j;
-        }
-        if (!node || j > i)
-        {
-            iterator_node_ = NULL;
-            //std::cout << "iterator_init NULL" << "\n";
-        }
-        else 
-        {
-            iterator_node_ = node;
-            //std::cout << "iterator_init " << iterator_node_->data_ << "\n";
-        }
-    }
-
-    bool iterator_end() const 
-    {
-        if (iterator_node_ == NULL)
-        {
-            //std::cout << " NULL " ;  
-            return true; 
-        }
-        else  
-        {
-            //std::cout << " NotNULL " ;  
-            return false;             
-        }
-    }
-    // 得到当前节点的值，并移到下一节点
-    T *iterator_next()
-    {
-        if (iterator_end())
-        {
-            //std::cout << " next NULL " ;
-            return NULL;
-        }
-        else 
-        {
-            T *t = &iterator_node_->data_;
-            iterator_node_ = iterator_node_->next_;
-            //std::cout << " next Not NULL " << *t << " " ;
-            return t;
-        }
-    }
-    // 反转链表
-    void reverse()
-    {
-        Node *list = head_.next_;
-        Node *head = NULL;
-        Node *next;
-
-        for (; list; list = next)
-        {
-            next = list->next_;
-            list->next_ = head;
-            head = list;
-        }
-
-        head_.next_ = head;
-    }
-
-    size_t size() const 
-    {
-        size_t cnt = 0;
-        Node *node = head_.next_;
-        while (node)
-        {
-            ++cnt;
-            node = node->next_;
-        }
-        return cnt;
-    }
-
-    void clear()
-    {
-        while (head_.next_)
-        {
-            Node *tmp = head_.next_;
-            head_.next_ = tmp->next_;
-            delete tmp;
-        }
-        head_.next_ = NULL;
-    }
-
-
+    void reverse();
+    size_t size() const;
+    void clear();
 private:
     struct Node 
     {
@@ -236,6 +57,200 @@ private:
     // 迭代器变量
     Node *iterator_node_;
 };
+// 头插法
+template<typename T>
+void LinkList<T>::push(const T &value)
+{
+    Node *tmp = new Node(value);
+    tmp->next_ = head_.next_;
+    head_.next_ = tmp;
+}
+template<typename T>
+void LinkList<T>::append(const T &value)
+{
+    Node *node = &head_;
+    while (node->next_)
+    {
+        node = node->next_;
+    }
+
+    Node *tmp = new Node(value);
+    node->next_ = tmp;
+}
+
+// 取第一个节点
+template<typename T>
+void LinkList<T>::pop(T &ret)
+{
+    Node *tmp = head_.next_;
+    head_.next_ = tmp->next_;
+    ret = tmp->data_;
+    delete tmp;
+}
+
+// 如果索引大于链表大小 直接把新节点插入链尾
+template<typename T>
+void LinkList<T>::insert(size_t i, const T &value)
+{
+    Node *node = &head_;
+    size_t j = 1;
+    while (node->next_ && j < i)
+    {
+        node = node->next_;
+        ++j;
+    }
+
+    Node *tmp = new Node(value);
+    if (!node->next_ || j > i) // 最后一个节点
+    {
+        node->next_ = tmp;
+    }
+    else 
+    {
+        tmp->next_ = node->next_->next_;
+        node->next_ = tmp;
+    }
+}
+
+// 删除索引处的节点
+template<typename T>
+void LinkList<T>::deletel(size_t i, T &ret)
+{
+    Node *node = &head_;
+    size_t j = 1;
+    while (node->next_ && j < i)
+    {
+        node = node->next_;
+        ++j;
+    }
+
+    if (!node->next_ || j > i) // 最后一个节点
+    {
+        return;
+    }  
+    else 
+    {
+        Node *tmp = node->next_;
+        node->next_ = tmp->next_;
+        ret = tmp->data_;
+        delete tmp;
+    }      
+}
+
+// 如果索引大于链表大小 直接返回最后节点
+template<typename T>
+inline
+T& LinkList<T>::get(size_t i)
+{
+    Node *node = &head_;
+    size_t j = 1;
+    while (node->next_ && j < i)
+    {
+        node = node->next_;
+        ++j;
+    }
+
+    if (!node->next_ || j > i)
+    {
+        return node->data_;
+    }
+    else 
+    {
+        return node->next_->data_ ;
+    }
+}
+
+// 单链表简单迭代器
+template<typename T>
+inline
+void LinkList<T>::iterator_begin(size_t i)
+{
+    Node *node = head_.next_;
+    size_t j = 1;
+    while (node && j < i)
+    {
+        node = node->next_;
+        ++j;
+    }
+    if (!node || j > i)
+    {
+        iterator_node_ = NULL;
+    }
+    else 
+    {
+        iterator_node_ = node;
+    }
+}
+template<typename T>
+inline
+bool LinkList<T>::iterator_end() const 
+{
+    if (iterator_node_ == NULL)
+    { 
+        return true; 
+    }
+    else  
+    { 
+        return false;             
+    }
+}
+// 得到当前节点的值，并移到下一节点
+template<typename T>
+typename LinkList<T>::iterator LinkList<T>::iterator_next()
+{
+    if (iterator_end())
+    {
+        return NULL;
+    }
+    else 
+    {
+        T *t = &iterator_node_->data_;
+        iterator_node_ = iterator_node_->next_;
+        return t;
+    }
+}
+// 反转链表
+template<typename T>
+inline
+void LinkList<T>::reverse()
+{
+    Node *list = head_.next_;
+    Node *head = NULL;
+    Node *next;
+
+    for (; list; list = next)
+    {
+        next = list->next_;
+        list->next_ = head;
+        head = list;
+    }
+
+    head_.next_ = head;
+}
+template<typename T>
+inline
+size_t LinkList<T>::size() const 
+{
+    size_t cnt = 0;
+    Node *node = head_.next_;
+    while (node)
+    {
+        ++cnt;
+        node = node->next_;
+    }
+    return cnt;
+}
+template<typename T>
+void LinkList<T>::clear()
+{
+    while (head_.next_)
+    {
+        Node *tmp = head_.next_;
+        head_.next_ = tmp->next_;
+        delete tmp;
+    }
+    head_.next_ = NULL;
+}
 
 
 #endif // !LINKLIST_H
--- PWDS/ch03/LinkList_test.cpp
+++ PWDS/ch03/LinkList_test.cpp
@@ -16,55 +16,37 @@ int main(void)
     l1.push(6);
     l1.push(1);
     std::cout << "list size: 3 --> " << l1.size() << "\n";
-    l1.insert(8,8);
+    l1.insert(8, 8);
     std::cout << "list insert(8,8) get(size): 8 --> " << l1.get(l1.size()) << "\n";
     std::cout << "list get(2): 6 --> " << l1.get(2) << "\n";
-    std::cout << "list pop(): 1 --> " << l1.pop() << "\n";
     int ret;
+    l1.pop(ret);
+    std::cout << "list pop(): 1 --> " << ret << "\n";
     l1.deletel(1, ret);
     std::cout << "list deletel(1): 6 --> " << ret << "\n";
     l1.append(9);
     std::cout << "list append(9): 9 --> " << l1.get(l1.size()) << "\n";
 
-    int *iter;
-    int count = 0;
-    l1.iterator_init(1);
-    //for (l1.iterator_init(1); l1.iterator_end(); )
-
-    if (l1.iterator_end())
+    l1.push(11);
+    l1.push(12);
+    l1.push(13);
+    LinkList<int>::iterator iter;
+    l1.iterator_begin(1);
+    while (!l1.iterator_end())
     {
-        std::cout << "list iterator_end NULL" << "\n";
-    } 
-    else  
-    {
-        std::cout << "list iterator_end Not NULL" << "\n";
         iter = l1.iterator_next();
-        std::cout << "list next " << *iter << "\n";
-        for (int i = 0; i < 2; ++i)
-        {
-            iter = l1.iterator_next();
-            if (iter != NULL)
-             std::cout << "list next " << *iter << "\n";
-            else 
-                break;
-        }
+        std::cout << *iter << " ";
     }
-    // while (l1.iterator_end())
-    // {
-    //     ++count;
-    //     std::cout << " count:" << count << " ";
-    //     iter = l1.iterator_next();
-    //     std::cout << *iter;
-    // }
-    // std::cout << "\n";
+    std::cout << "\n";
 
-    // l1.reverse();
-    // for (l1.iterator_init(1); l1.iterator_end(); )
-    // {
-    //     iter = l1.iterator_next();
-    //     std::cout << *iter;
-    // }
-    // std::cout << "\n";    
+    // 翻转链表
+    l1.reverse();
+    for (l1.iterator_begin(1); !l1.iterator_end(); )
+    {
+        iter = l1.iterator_next();
+        std::cout << *iter << " ";
+    }
+    std::cout << "\n";    
 
     //std::cout << "list find(6): true -->  " << (l1.find(6) == 0 ? "false" : "true") << "\n";
 
