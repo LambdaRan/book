12833925eb8c4b7d3362f4af82e6b7a3eb4322ce
LambdaRan <272045023@qq.com>
2018-04-16 01:12:34 +0800

    Notebook add StaticList 2018-04-16 01:12:31

--- PWDS/ch03/StaticList.h
+++ PWDS/ch03/StaticList.h
@@ -1,4 +1,11 @@
 
+// 静态链表: 用数组描述的链表 (游标实现法)
+
+// 未被使用的数组元素为备用链表
+// 数组第一个元素（下标为0）的cur 存放备用链表的第一个节点的下标
+// 数组最后一个元素的cur用来存放第一个插入元素的下标，相当于头节点
+// 已存储数据的最后一个元素的cur的值为0
+
 
 
 
@@ -6,36 +13,204 @@
 #define STATICLIST_H
 
 #include <stddef.h>
+#include <assert.h>
 
 template<typename T, size_t S>
 class StaticList 
 {
 public: 
     StaticList()
-        : capcity_(S)
+        : size_(0), capcity_(S)
     {
-        for (int i = 0; i < capcity_-1; ++i)
-        {
-            container_[i].cur_ = i + 1;
-        }
-        container_[capcity_-1].cur_ = 0;
+        _init();
+    }
+    ~StaticList()
+    {
+        clear();
+    }
+    // 在第i个元素之前插入新的元素
+    // 如果i大于已存储节点的下标，则存储在最后
+    // 成功返回true否则false  
+    bool insert(size_t i, const T &value); 
+
+    // 删除第i个元素,如果i大于size则不删除任何元素
+    void deletel(size_t i, T &ret);
+
+    void push(const T &value);
+    void push_back(const T &value);
+    // 如果 i 大于size，返回最后一个元素的值
+    T& get(size_t i);    
+    // 清空链表
+    void clear()
+    {
+        _init();
+        size_ = 0;
+    }
+    size_t size() const 
+    {
+        return size_;
     }
 
-private: 
+    // 迭代器
+
+
+private:
     struct Component
     {
-        Component(const T &value)
-            : data_(value), cur_(0)
+        Component()
+            : cur_(0)
         {}
 
         T data_; // 数据
         size_t cur_; // 游标，为0表示无指向
     };
 
-    Component container_[S];
+    void _init();
+    // 若备用链表非空，则返回分配的节点下标，否则返回0
+    size_t _malloc_sl();
+    void _free_sl(size_t i);
+
+    size_t size_;
     size_t capcity_;
+    Component container_[S];
 };
 
+template<typename T, size_t S>
+inline
+void StaticList<T,S>::_init()
+{
+    for (size_t i = 0; i < capcity_-1; ++i)
+    {
+        container_[i].cur_ = i + 1;
+    }
+    container_[capcity_-1].cur_ = 0;        
+}
+template<typename T, size_t S>
+inline
+size_t StaticList<T,S>::_malloc_sl()
+{
+    size_t i = container_[0].cur_;
+    if (container_[0].cur_) // 备用链表不空
+    {
+        container_[0].cur_ = container_[i].cur_;
+    }
+    return i;
+}
+template<typename T, size_t S>
+inline
+void StaticList<T,S>::_free_sl(size_t i)
+{
+    assert(i <= capcity_-1);
 
+    container_[i].cur_ = container_[0].cur_;
+    container_[0].cur_ = i;
+}
+
+template<typename T, size_t S>
+inline
+bool StaticList<T,S>::insert(size_t i, const T &value)
+{
+    if (i < 1)
+    {
+        return false;
+    }
+    if (i > size_+1)
+    {
+        push_back(value);
+        return true;
+    }
+    size_t k = capcity_ - 1;// 头节点下标
+    size_t j = _malloc_sl();
+    if (j)
+    {
+        container_[j].data_ = value;
+        for (size_t l = 1; l <= i-1; ++l)
+        {
+            k = container_[k].cur_;
+        }
+        container_[j].cur_ = container_[k].cur_;
+        container_[k].cur_ = j;
+        ++size_;
+
+        return true;
+    }
+    return false;
+}
+
+template<typename T, size_t S>
+inline
+void StaticList<T,S>::deletel(size_t i, T &ret)
+{
+    if (i < 1 || i > size_+1)
+    {
+        return ;
+    }
+    size_t k = capcity_ - 1;// 头节点下标
+    for (size_t j = 0; j <= i-1; ++j)
+    {
+        k = container_[k].cur_;
+    }
+
+    size_t j = container_[k].cur_;
+    container_[k].cur_ = container_[j].cur_;
+    ret = container_[j].data_;
+    _free_sl(j);
+    --size_;
+}
+
+template<typename T, size_t S>
+inline
+void StaticList<T,S>::push(const T &value)
+{
+    size_t j = _malloc_sl();
+    if (j) // 头插法插入元素
+    {
+        container_[j].data_ = value;
+        container_[j].cur_ = container_[capcity_ - 1].cur_;
+        container_[capcity_ - 1].cur_ = j;
+        ++size_;
+    }
+}
+template<typename T, size_t S>
+inline
+void StaticList<T,S>::push_back(const T &value)
+{
+    size_t j = _malloc_sl();
+    if (j) // 插到最后
+    {
+        size_t k = capcity_ - 1;
+        while (container_[k].cur_)
+        {
+            k = container_[k].cur_;
+        }
+        container_[j].data_ = value;
+        container_[k].cur_ = j;
+        ++size_;
+    }
+}
+template<typename T, size_t S>
+inline
+T& StaticList<T,S>::get(size_t i)
+{
+    assert(i <= size_);
+
+    //int k = container_[capcity_ - 1].cur_;
+    int k = capcity_ - 1;
+    int j = 1;
+    //while (k && j < i)
+    while (container_[k].cur_ && j < i)
+    {
+        k = container_[k].cur_;
+        ++j;
+    }
+    if (container_[k].cur_ == 0 || j > i)
+    {
+        return container_[k].data_;
+    }
+    else  
+    {
+        return container_[container_[k].cur_].data_;
+    }
+}
 
 #endif // !STATICLIST_H
---
+++ PWDS/ch03/StaticList_test.cpp
@@ -0,0 +1,61 @@
+
+
+
+#include "StaticList.h"
+
+#include <iostream>
+
+
+int main(void)
+{
+
+    StaticList<int, 32> l1;
+
+    std::cout << "list size: 0 -- > " << l1.size() << std::endl;
+
+    l1.push(2);
+    l1.push(6);
+    l1.push(1);
+    std::cout << "list size: 3 --> " << l1.size() << "\n";
+    l1.insert(8, 8);
+    std::cout << "list insert(8,8) get(size): 8 --> " << l1.get(l1.size()) << "\n";
+    std::cout << "list get(2): 6 --> " << l1.get(2) << "\n";
+    int ret;
+    //l1.pop(ret);
+    //std::cout << "list pop(): 1 --> " << ret << "\n";
+    l1.deletel(1, ret);
+    std::cout << "list deletel(1): 6 --> " << ret << "\n";
+    //l1.append(9);
+    //std::cout << "list append(9): 9 --> " << l1.get(l1.size()) << "\n";
+
+    // l1.push(11);
+    // l1.push(12);
+    // l1.push(13);
+    // LinkList<int>::iterator iter;
+    // l1.iterator_begin(1);
+    // while (!l1.iterator_end())
+    // {
+    //     iter = l1.iterator_next();
+    //     std::cout << *iter << " ";
+    // }
+    // std::cout << "\n";
+
+    // // 翻转链表
+    // l1.reverse();
+    // for (l1.iterator_begin(1); !l1.iterator_end(); )
+    // {
+    //     iter = l1.iterator_next();
+    //     std::cout << *iter << " ";
+    // }
+    // std::cout << "\n";    
+
+    //std::cout << "list find(6): true -->  " << (l1.find(6) == 0 ? "false" : "true") << "\n";
+
+    //int ret = l1.deletel(0);
+    //std::cout << "list deletel(0) value: 0 -->  " << ret << "\n";
+
+    l1.clear();
+    std::cout << "list clear size: 0 --> " << l1.size() << "\n";
+
+    return 0;
+}
