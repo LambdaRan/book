7c7edae9c448517fab318bdd0a7ce98f4c9ded27
Lambda <2861429309@qq.com>
2018-04-15 16:10:40 +0800

    Desktop add 2018-04-15 16:10:32

---
+++ EffectiveC++/test04.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+inline int get_static()
+{
+    static int loc = 3;
+    return (++loc);
+}
+int main()
+{
+    std::cout << "Hello Effective C++ \n";
+    std::cout << "value1: " << get_static() << "\n";
+    std::cout << "value2: " << get_static() << "\n";
+    return 0;
+}
