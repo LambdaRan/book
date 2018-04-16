f19eeb36541d56e0f91a3b63f7ff6550002fcf25
Lambda <2861429309@qq.com>
2018-04-16 21:36:12 +0800

    Desktop add 2018-04-16 21:36:04

---
+++ HPLSP/ch05/test_510.c
@@ -0,0 +1,54 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+#include <libgen.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 512
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("Usage: %s ip_address port_number send_buffer_size\n", basename(argv[0]));
+        return 1;
+    }
+
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &server_address.sin_addr);
+    server_address.sin_port = htons(port);
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    int sendbuf = atoi(argv[3]);
+    int len = sizeof(sendbuf);
+    // 先设置ＴＣＰ发送缓冲区的大小，然后立即读取之
+    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, sizeof(sendbuf));
+    getsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, (socklen_t *)&len);
+    printf("the tcp send buffer size after setting is %d\n", sendbuf);
+
+    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) != -1)
+    {
+        char buffer[BUFFER_SIZE];
+        memset(buffer, 0, BUFFER_SIZE);
+        send(sock, buffer, BUFFER_SIZE, 0);
+    }
+
+    close(sock);
+    return 0;
+}
