d1eefe0b15798ca971fd6eeb7014a3b6584a8452
LambdaRan <272045023@qq.com>
2018-08-01 00:21:21 +0800

    HPLSP add ch09

---
+++ HPLSP/ch09/main913.c
@@ -0,0 +1,94 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <errno.h>
+
+
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <libgen.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+        return 0;
+    }
+
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+    
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+    ret = bind(listenfd, (struct sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    struct sockaddr_in client_address;
+    socklen_t client_addrlen = sizeof(client_address);
+    int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_addrlen);
+    if (connfd < 0)
+    {
+        printf("errno is: %d\n", errno);
+        close(listenfd);
+    }
+
+    char buf[1024];
+    fd_set read_fds;
+    fd_set exception_fds;
+    FD_ZERO(&read_fds);
+    FD_ZERO(&exception_fds);
+
+    while (true)
+    {
+        memset(buf, '\0', sizeof(buf));
+
+        FD_SET(connfd, &read_fds);
+        FD_SET(connfd, &exception_fds);
+
+        ret = select(connfd+1, &read_fds, NULL, &exception_fds, NULL);
+        if (ret < 0)
+        {
+            printf("selection failure\n");
+            break;
+        }
+        if (FD_ISSET(connfd, &read_fds))
+        {
+            ret = recv(connfd, buf, sizeof(buf)-1, 0);
+            if (ret < 0)
+                break;
+            printf("get %d bytes of normal data: %s\n", ret, buf);
+        }
+        else if (FD_ISSET(connfd, &exception_fds))
+        {
+            ret = recv(connfd, buf, sizeof(buf)-1, MSG_OOB);
+            if (ret <= 0)
+                break;
+            printf("get %d bytes of oob data: %s\n", ret, buf);
+        }
+    }
+    close(connfd);
+    close(listenfd);
+
+    return 0;
+}
